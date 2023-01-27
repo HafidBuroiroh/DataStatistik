3ad3b6045a918a0c84dab357421866b4952f4156
HafidBuroiroh <hafidburoiroh09@gmail.com>
2023-01-27 10:00:44 +0700

    fist Commit

---
+++ DataStatistik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataStatistik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
